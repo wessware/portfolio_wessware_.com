56dd0711db366a993f52cef7c5138021fef3a3fd
wessware <stevenshisia@yahoo.com>
2022-11-22 13:23:25 +0300

    data fetch

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/577210/pexels-photo-577210.jpeg",
+        link: "https://github.com/wessware/"
+    },
+    {
+        id: 2,
+        img: 'https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg',
+        link: "https://github.com/wessware/"
+    },
+    {
+        id: 3,
+        img: 'https://images.pexels.com/photos/590022/pexels-photo-590022.jpeg',
+        link: "https://github.com/wessware/"
+    },
+    {
+        id: 4,
+        img: 'https://images.pexels.com/photos/669619/pexels-photo-669619.jpeg',
+        link: "https://github.com/wessware/"
+    },
+    {
+        id: 5,
+        img: 'https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg',
+        link: "https://github.com/wessware/"
+    },
+    {
+        id: 6,
+        img: 'https://images.pexels.com/photos/95916/pexels-photo-95916.jpeg',
+        link: "https://github.com/wessware/"
+    }
+]
